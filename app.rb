039ac93a5f26cb32ca2dc2dea7c4b64d2285ffb5
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-04-16 08:53:48 -0300

    add gs, dep and add some html into root

--- app.rb
+++ app.rb
@@ -3,5 +3,17 @@ require "cuba"
 Cuba.define do
   on root do
     res.write("Hello Frogger!")
+    res.write("
+      <!DOCTYPE html>
+        <html>
+          <body>
+
+            <p>This is a paragraph.</p>
+            <p>This is a paragraph.</p>
+            <p>This is a paragraph.</p>
+
+          </body>
+        </html>
+      ")
   end
 end
